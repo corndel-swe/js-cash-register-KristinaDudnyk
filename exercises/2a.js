53ab5079bdc9a44eb620f3e26d5bc29d4fbeb7c6
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2024-11-29 13:56:40 +0000

    exercises

--- exercises/2a.js
+++ exercises/2a.js
@@ -2,25 +2,33 @@
  * Given a list of recipes, return the number of vegetarian recipes in the list
  */
 function countVeggies(recipes) {
-  // code here
+  let vegyRecipes = 0;
+
+  for (let r of recipes) {
+    if (r.isVegetarian === true) {
+      vegyRecipes += 1;
+    }
+  }
+
+  return vegyRecipes;
 }
 
 // array provided for debugging:
 const recipes = [
   {
-    name: 'Spaghetti Bolognese',
-    ingredients: ['spaghetti', 'beef', 'tomato sauce'],
+    name: "Spaghetti Bolognese",
+    ingredients: ["spaghetti", "beef", "tomato sauce"],
     servings: 4,
-    isVegetarian: false
+    isVegetarian: false,
   },
 
   {
-    name: 'Vegetable Stir Fry',
-    ingredients: ['broccoli', 'carrots', 'bell peppers', 'soy sauce'],
+    name: "Vegetable Stir Fry",
+    ingredients: ["broccoli", "carrots", "bell peppers", "soy sauce"],
     servings: 2,
-    isVegetarian: true
-  }
-]
+    isVegetarian: true,
+  },
+];
 
 // don't change below
-export default countVeggies
+export default countVeggies;
